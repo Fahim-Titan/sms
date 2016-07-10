9b47df5940051bbafd665809494fa8bb2e6bb28e
Tanvir Iqbal <tanviriqbal4u@live.com>
2016-07-10 12:17:55 +0600

    File page design

--- resources/views/files.blade.php
+++ resources/views/files.blade.php
@@ -1,24 +1,44 @@
-you are signed in as {{Auth::user()->name}}
-{{--{{Auth::id()}}--}}
-
-
+<!DOCTYPE html>
+<html>
+<head>
+	<title>@yield('title')</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Bootstrap css file-->
+	<link rel="stylesheet" href="{{ URL::to('src/css/bootstrap.min.css')}}">
+	<style>
+		.my-form-size
+		{
+			width: 50%;
+		}
+	</style>
+</head>
+<body>
+<div class="container">
+	<h1 class="text-center">Add Files</h1>
+	<hr>
 {!! Form::open(array('route' => 'file.store', 'method' => 'POST', 'files' => true)) !!}
-	<ul>
-		<li>
-			{!! Form::label('file_path', 'File_path:') !!}
-			{!! Form::file('file_path') !!}
-		</li>
-		<li>
+		<div class="form-group">
+			{!! Form::label('file_path', 'File path:') !!}
+			{!! Form::file('file_path',null,['class' => 'form-control']) !!}
+		</div>
+		<div class="form-group">
 			{!! Form::label('date', 'Date:') !!}
-			{!! Form::date('date') !!}
-		</li>
-		<li>
-			{!! Form::label('type', 'Type:') !!}
-			{!! Form::select('type', array('notice'=> 'Notices', 'files' => 'Files'), null, ['placeholder' => 'Select your roles'])!!}
-		</li>
+			{!! Form::date('date',null,['class' => 'form-control']) !!}
+		</div>
+		<div class="form-group">
+			{!! Form::label('type', 'Type:') !!} <br>
+			{!! Form::select('type', array('notice'=> 'Notices', 'files' => 'Files'), null, ['placeholder' => 'Select your roles', 'class' => 'form-control'])!!}
+		</div>
 
-		<li>
-			{!! Form::submit() !!}
-		</li>
-	</ul>
+		<div class="form-group">
+			{!! Form::submit(null, ['class' => 'btn btn-default']) !!}
+		</div>
 {!! Form::close() !!}
+</div>
+<!-- initialize jQuery Library -->
+<script src="{{ URL::to('src/js/jequery.min.js')}}"></script>
+<!-- Bootstrap js -->
+<script src="{{ URL::to('src/js/bootstrap.min.js')}}"></script>
+</body>
+</html>
