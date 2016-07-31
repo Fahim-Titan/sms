30af4802123aaab10aac8b45bdf363aa5f60d67d
tanvirIqbal <tanviriqbal4u@live.com>
2016-07-31 15:56:40 +0600

    Form Header Design

--- resources/views/subjects.blade.php
+++ resources/views/subjects.blade.php
@@ -11,9 +11,61 @@
 		{
 			width: 50%;
 		}
+		
+		div.card {
+            width: 250px;
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+            /*text-align: center;*/
+            padding: 10px;
+        }
+		
+
+  .navbar {
+      
+      background-color: #f4511e;
+      z-index: 9999;
+      
+      font-size: 12px !important;
+      line-height: 1.42857143 !important;
+      letter-spacing: 4px;
+      border-radius: 0;
+      font-family: Montserrat, sans-serif;
+  }
+  .navbar li a, .navbar .navbar-brand {
+      color: #fff !important;
+  }
+  .navbar-nav li a:hover, .navbar-nav li.active a {
+      color: #f4511e !important;
+      background-color: #fff !important;
+  }
+  .navbar-default .navbar-toggle {
+      border-color: transparent;
+      color: #fff !important;
+  }
+  footer .glyphicon {
+      font-size: 20px;
+      margin-bottom: 20px;
+      color: #f4511e;
+  }
+  @media screen and (max-width: 768px) {
+    .col-sm-4 {
+      text-align: center;
+      margin: 25px 0;
+    }
+    .btn-lg {
+        width: 100%;
+        margin-bottom: 35px;
+    }
+  }
+  @media screen and (max-width: 480px) {
+    .logo {
+        font-size: 150px;
+    }
+  }
 	</style>
 </head>
 <body>
+@include('includes.dashboard_header')
 <div class="container">
 <h1 class="text-center">Add Subject</h1>
 <hr>
--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -11,9 +11,61 @@
 		{
 			width: 50%;
 		}
+		
+		 div.card {
+            width: 250px;
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+            /*text-align: center;*/
+            padding: 10px;
+        }
+		
+
+  .navbar {
+      
+      background-color: #f4511e;
+      z-index: 9999;
+      
+      font-size: 12px !important;
+      line-height: 1.42857143 !important;
+      letter-spacing: 4px;
+      border-radius: 0;
+      font-family: Montserrat, sans-serif;
+  }
+  .navbar li a, .navbar .navbar-brand {
+      color: #fff !important;
+  }
+  .navbar-nav li a:hover, .navbar-nav li.active a {
+      color: #f4511e !important;
+      background-color: #fff !important;
+  }
+  .navbar-default .navbar-toggle {
+      border-color: transparent;
+      color: #fff !important;
+  }
+  footer .glyphicon {
+      font-size: 20px;
+      margin-bottom: 20px;
+      color: #f4511e;
+  }
+  @media screen and (max-width: 768px) {
+    .col-sm-4 {
+      text-align: center;
+      margin: 25px 0;
+    }
+    .btn-lg {
+        width: 100%;
+        margin-bottom: 35px;
+    }
+  }
+  @media screen and (max-width: 480px) {
+    .logo {
+        font-size: 150px;
+    }
+  }
 	</style>
 </head>
 <body>
+@include('includes.dashboard_header')
 <div class="container">
 	<h1 class="text-center">Add User</h1>
 	<hr>
@@ -56,9 +108,9 @@
 		{!! Form::text('password',null,['class' => 'form-control']) !!}
 	</div>
 	<div class="form-group">
-		{!! Form::label('image', 'Image:') !!}
 		{!! Form::file('image',null,['class' => 'form-control']) !!}
-	</div>
+	</div>:label('image', 'Image:') !!}
+		{!! Form:
 	<div class="form-group">
 		{!! Form::label('qualification', 'Qualification:') !!}
 		{!! Form::text('qualification',null,['class' => 'form-control']) !!}
@@ -83,6 +135,7 @@
 
 {!! Form::close() !!}
 	</div>
+	<!--@include('includes.dashboard_footer')-->
 <!-- initialize jQuery Library -->
 <script src="{{ URL::to('src/js/jequery.min.js')}}"></script>
 <!-- Bootstrap js -->
